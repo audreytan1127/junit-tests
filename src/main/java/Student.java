9a8c27dd265ee48e80e71e20b473bce8281e40f2
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-16 14:09:40 -0500

    completed first part of the junit exercises

---
+++ src/main/java/Student.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student() {
+    }
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getID() {
+        this.id = id;
+        return id;
+    }
+
+    public String getName() {
+        this.name = name;
+        return name;
+    }
+
+    public int addGrade(int grade) {
+        this.grades.add(grade);
+        return grade;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public double getGradeAverage(){
+            int sumOfGrades = 0;
+            for (Integer grade : grades) {
+                sumOfGrades += grade;
+            }
+            double averageGrade = (double) sumOfGrades / grades.size();
+            return averageGrade;
+    }
+
+}
